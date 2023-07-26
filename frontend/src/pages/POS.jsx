463668da79c9ad722c99efdd1c90da282b9d7717
cleovilakazi <cleopathrastudies@gmail.com>
2023-07-26 17:53:25 +0200

    Stuck

--- frontend/src/pages/POS.jsx
+++ frontend/src/pages/POS.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 const POS = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [cart, setCart] = useState([]);
+  let [cart, setCart] = useState([]);
 
   const fetchProducts = async () => {
     setIsLoading(true);
@@ -14,39 +14,41 @@ const POS = () => {
     setIsLoading(false);
   };
 
-  const addProductToCart = async(product) => {
+  const addProductToCart = async(product) =>{
+    // check if the adding product exist
     let findProductInCart = await cart.find(i=>{
       return i.id === product.id
-    })
+    });
+
     if(findProductInCart){
       let newCart = [];
       let newItem;
+
       cart.forEach(cartItem => {
         if(cartItem.id === product.id){
           newItem = {
             ...cartItem,
-            quantity: cartItem.quantity +1,
+            quantity: cartItem.quantity + 1,
             totalAmount: cartItem.price * (cartItem.quantity + 1)
           }
           newCart.push(newItem);
+        }else{
+          newCart.push(cartItem);
         }
+      });
 
-      })
+      setCart(newCart);
+      
 
-
-    }
-    else{
+    }else{
       let addingProduct = {
         ...product,
         'quantity': 1,
-        'totalAmount': product.price
-
+        'totalAmount': product.price,
       }
-      setCart([...cart,addingProduct])
+      setCart([...cart, addingProduct]);
+      
     }
-    
-
-
 
   }
 
@@ -58,13 +60,16 @@ const POS = () => {
     <MainLayout>
       <div className="row">
         <div className="col-lg-8">
-          {isLoading ? (
+          {isLoading ? 
             "Loading"
-          ) : (
+           : (
             <div className="row">
               {products.map((product, key) => (
                 <div key={key} className="col-lg-4">
-                  <div className="border" onClick={() => addProductToCart(product)}>
+                  <div
+                    className="border"
+                    onClick={() => addProductToCart(product)}
+                  >
                     <p>{product.name}</p>
                     <img
                       src={product.image}
@@ -78,7 +83,40 @@ const POS = () => {
             </div>
           )}
         </div>
-        <div className="col-lg-4"></div>
+        <div className="col-lg-4">
+          <div className="table-responsive bg-dark">
+            <table className="table table-responsive table-dark table-hover">
+              <thead>
+                <tr>
+                  <td>#</td>
+                  <td>Name</td>
+                  <td>Price</td>
+                  <td>Qty</td>
+                  <td>Total</td>
+                  <td>Action</td>
+                </tr>
+              </thead>
+              <tbody>
+                {cart
+                  ? cart.map((cartProduct, key) => 
+                      <tr key={key}>
+                        <td>{cartProduct.id}</td>
+                        <td>{cartProduct.name}</td>
+                        <td>{cartProduct.price}</td>
+                        <td>{cartProduct.quantity}</td>
+                        <td>{cartProduct.totalAmount}</td>
+                        <td>
+                          <button className="btn btn-danger btn-sm">
+                            Remove
+                          </button>
+                        </td>
+                      </tr>
+                    )
+                  : "No item in cart"}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </MainLayout>
   );
