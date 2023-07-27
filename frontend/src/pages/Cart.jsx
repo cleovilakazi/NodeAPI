bb3455ea829ac5baefba9860a9dd87f7084aefa1
cleovilakazi <cleopathrastudies@gmail.com>
2023-07-27 14:39:45 +0200

    Remove button works

---
+++ frontend/src/pages/Cart.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+
+const Cart = () => {
+  const [cart, setCart] = useState([]);
+  const [totalAmount, setTotalAmount] = useState(0);
+};
+
+export default Cart;
--- frontend/src/pages/POS.jsx
+++ frontend/src/pages/POS.jsx
@@ -5,7 +5,8 @@ import axios from "axios";
 const POS = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  let [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [totalAmount, setTotalAmount] = useState(0);
 
   const fetchProducts = async () => {
     setIsLoading(true);
@@ -14,57 +15,63 @@ const POS = () => {
     setIsLoading(false);
   };
 
-  const addProductToCart = async(product) =>{
-    // check if the adding product exist
-    let findProductInCart = await cart.find(i=>{
-      
-      return i.id === product.id
+  const addProductToCart = async (product) => {
+    let findProductInCart = await cart.find((i) => {
+      return i.id === product.id;
     });
 
-    if(findProductInCart){
+    if (findProductInCart) {
       let newCart = [];
       let newItem;
 
-      cart.forEach(cartItem => {
-        if(cartItem.id === product.id){
+      cart.forEach((cartItem) => {
+        if (cartItem.id === product.id) {
           newItem = {
             ...cartItem,
             quantity: cartItem.quantity + 1,
-            totalAmount: cartItem.price * (cartItem.quantity + 1)
-          }
+            totalAmount: cartItem.price * (cartItem.quantity + 1),
+          };
           newCart.push(newItem);
-        }
-        else{
+        } else {
           newCart.push(cartItem);
         }
       });
 
       setCart(newCart);
-      
-
-    }else{
+    } else {
       let addingProduct = {
         ...product,
-        'quantity': 1,
-        'totalAmount': product.price,
-      }
+        quantity: 1,
+        totalAmount: product.price,
+      };
       setCart([...cart, addingProduct]);
-      
     }
+  };
 
-  }
+  const removeProduct = async (product) => {
+    const newCart = cart.filter((cartItem) => cartItem.id !== product.id);
+    setCart(newCart);
+  };
 
   useEffect(() => {
     fetchProducts();
   }, []);
 
+  useEffect(() => {
+    let newTotalAmount =0
+    cart.forEach(cartItem => {
+      newTotalAmount += cartItem.to
+    })
+    setTotalAmount(newTotalAmount)
+  }, [cart]);
+
   return (
     <MainLayout>
       <div className="row">
         <div className="col-lg-8">
-          {isLoading ? 
+          {isLoading ? (
             "Loading"
-           : (
+          ) : (
             <div className="row">
               {products.map((product, key) => (
                 <div key={key} className="col-lg-4">
@@ -100,7 +107,7 @@ const POS = () => {
               </thead>
               <tbody>
                 {cart
-                  ? cart.map((cartProduct, key) => 
+                  ? cart.map((cartProduct, key) => (
                       <tr key={key}>
                         <td>{cartProduct.id}</td>
                         <td>{cartProduct.name}</td>
@@ -108,15 +115,19 @@ const POS = () => {
                         <td>{cartProduct.quantity}</td>
                         <td>{cartProduct.totalAmount}</td>
                         <td>
-                          <button className="btn btn-danger btn-sm">
+                          <button
+                            className="btn btn-danger btn-sm"
+                            onClick={() => removeProduct(cartProduct)}
+                          >
                             Remove
                           </button>
                         </td>
                       </tr>
-                    )
+                    ))
                   : "No item in cart"}
               </tbody>
             </table>
+            <h3 className="px-2 text-white">R{totalAmount}</h3>
           </div>
         </div>
       </div>
