ba24d518deab18e0f9ff2336bc952735d16bae0d
cleovilakazi <cleopathrastudies@gmail.com>
2023-08-10 14:23:16 +0200

    Navigation works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 import HompePage from './pages/HompePage';
 import POS from './pages/POS';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
 function App() {
+  const {user} = useAuthContext()
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<HompePage />} />
-          <Route path="/pos" element={<POS />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/" element={!user ? <HompePage /> : <Navigate to="/pos"/>} />
+          <Route path="/pos" element={user ? <POS /> : <Navigate to= "/"/>} />
+          <Route path="/login" element={!user ?<Login /> : <Navigate to="/pos"/>} />
+          <Route path="/register" element={!user ? <Register /> : <Navigate to="/pos"/>} />
         </Routes>
       </Router>
     </div>
